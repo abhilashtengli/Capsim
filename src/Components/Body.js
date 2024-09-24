65b8d376f25e343a3b79babf319d6d39d34bf891
Abhilash <abhilasht0007.t@gmail.com>
2024-09-24 12:04:56 +0530

    Capsim

---
+++ src/Components/Body.js
@@ -0,0 +1,50 @@
+import React, { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Component/Home";
+import AboutUs from "./Component/AboutUs";
+import Product from "./Component/Product";
+import Contact from "./Component/Contact";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<div>Home page Loading...</div>}>
+          <Home />{" "}
+        </Suspense>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<div>About Us Loading...</div>}>
+          <AboutUs />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<div>Product page Loading...</div>}>
+          <Product />
+        </Suspense>
+      ),
+    },
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<div></div>}>
+          <Contact />{" "}
+        </Suspense>
+      ),
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
